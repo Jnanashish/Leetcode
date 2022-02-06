4337734152748d7713aa437a2932f2e61dfdca32
Jnanashish Handique <42514311+Jnanashish@users.noreply.github.com>
2022-02-06 20:39:34 +0530

    Time: 166 ms (67.20%), Space: 91.4 MB (84.40%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -3,20 +3,21 @@ public:
     int arrayNesting(vector<int>& nums) {
         int n = nums.size() - 1;
         int ans = 0;
-        vector<int> v(n+1, 0);
-        for(int i = 0; i<=n; i++){
+    
+        for(int i = 0; i<=n; i++){ 
+            if(nums[i] != -1){
+                int cnt = 0;
+                int a = i;
             
-            int cnt = 0;
-            int a = i;
-            
-            while(true){
-                a = nums[a];
-                if(v[a]) 
-                    break;
-                cnt++;
-                v[a] = 1;
+                while(nums[a] != -1){
+                   // cout << a << " " <<nums[a]<<" "<<endl;
+                    int temp = a;
+                    a = nums[a];
+                    cnt++;
+                    nums[temp] = -1;
+                }
+                ans = max(ans, cnt);
             }
-            ans = max(ans, cnt);
         }
         return ans;
     }
