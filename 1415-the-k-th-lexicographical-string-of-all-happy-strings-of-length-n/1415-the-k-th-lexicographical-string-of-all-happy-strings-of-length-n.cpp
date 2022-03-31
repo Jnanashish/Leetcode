c1f79222e0632652abff7dfef1c622c23ecbef63
Jnanashish Handique <42514311+Jnanashish@users.noreply.github.com>
2022-03-31 12:47:36 +0530

    Time: 28 ms (40.46%), Space: 5.9 MB (86.34%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    vector<string> ans;
-    
-    void gensub(string s, int n, string res = ""){
+    string ans;
+    int cnt = 0;
+    void gensub(string s, int n, int k, string res = ""){
         if(res.length() == n){
-            ans.push_back(res);
+            cnt = cnt+1;
+            
+            if(cnt == k){
+                ans = res;
+                return;
+            }
             return;
         }
         
         for(int i = 0; i<s.length(); i++){
             int t = res.length();
             if(t > 0 and res[t-1] != s[i]){
-                gensub(s, n, res+s[i]);
+                gensub(s, n, k, res+s[i]);
             } else if(t == 0){
-                gensub(s, n, res+s[i]);
+                gensub(s, n, k, res+s[i]);
             }
         }
         return;    
@@ -21,7 +26,7 @@ public:
     
     string getHappyString(int n, int k) {
         string s = "abc";
-        gensub(s, n);
-        return ans.size() >= k ? ans[k-1] : "";
+        gensub(s, n, k);
+        return ans.length() > 0 ? ans : "";
     }
 };
