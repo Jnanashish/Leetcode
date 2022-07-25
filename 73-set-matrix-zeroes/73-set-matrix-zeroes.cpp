4b9f44f46421e6487c45a41049cbfe396e18091b
Jnanashish Handique <42514311+Jnanashish@users.noreply.github.com>
2022-07-25 09:15:45 +0530

    Time: 22 ms (53.24%), Space: 13.3 MB (18.74%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) { 
-	int row = matrix.size();
-	int col = matrix[0].size();
-	
-	int rowarr[201] = {0};
-    int  colarr[201] = {0};
- 
-	
-	for(int i = 0; i<row; i++)
-		for(int j = 0; j<col; j++){
-			if(matrix[i][j] == 0){
-				rowarr[i] = 1;
-				colarr[j] = 1;
-			}
-		}
-	
-	
-	for(int i = 0; i<row; i++)
-		for(int j = 0; j<col; j++){
-			if(rowarr[i] == 1 || colarr[j] == 1)
-				matrix[i][j] = 0;
-		}
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_map<int, int> um_col;
+        unordered_map<int, int> um_row;
+        
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j = 0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 0){
+                    um_col[j]++;
+                    um_row[i]++;
+                }
+            }
+        }
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j = 0; j<matrix[0].size(); j++){
+                if(um_col.find(j) != um_col.end() || um_row.find(i) != um_row.end()){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
     }
 };
